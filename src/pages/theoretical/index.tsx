c1fd845ba9280f61f203ae53845b5e2b25272763
Jo Egner <14022395+vechema@users.noreply.github.com>
2022-08-11 18:34:20 -0500

    Theoretical topic outline

--- src/pages/theoretical/index.tsx
+++ src/pages/theoretical/index.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Typography from '@mui/material/Typography';
 import TheoryNav from '../../components/theoryNav';
+import { List, ListItem } from '@mui/material';
 
 const TheoreticalPage = ({ data, location }) => {
   return (
@@ -10,6 +11,37 @@ const TheoreticalPage = ({ data, location }) => {
       <>
         <TheoryNav />
         <Typography>Some pearls of wisdom that I try to code by</Typography>
+
+        <Typography variant="h3">Non Functional Requirements</Typography>
+        <List>
+          <ListItem>Simplicity (Simple vs Easy)</ListItem>
+          <ListItem>Maintainability</ListItem>
+          <ListItem>Readability</ListItem>
+          <ListItem>Testability</ListItem>
+          <ListItem>Idempotency</ListItem>
+          <ListItem>Proper abstaction, modularity, single responsibility</ListItem>
+          <ListItem>Repeatability - Cattle, not pets</ListItem>
+        </List>
+
+        <Typography variant='h3'>Functional Programming</Typography>
+        <List>
+          <ListItem>Data first, pure</ListItem>
+          <ListItem>Immutability, static not dynamic, change is the root of all evil</ListItem>
+          <ListItem>Lazy - derive vs denormalize</ListItem>
+          <ListItem>Stateless</ListItem>
+          <ListItem>Entity/Component/System</ListItem>
+        </List>
+
+        <Typography variant='h3'>Misc</Typography>
+        <List>
+          <ListItem>Problem first, solution after</ListItem>
+          <ListItem>Strong opinions, weakly held</ListItem>
+          <ListItem>Create new better, replace old later/maybe never</ListItem>
+          <ListItem>Synchronous</ListItem>
+          <ListItem>Measure everything</ListItem>
+          <ListItem>Premature optimization</ListItem>
+          <ListItem>Composition over Inheritance</ListItem>
+        </List>
       </>
     </Layout>
   )
